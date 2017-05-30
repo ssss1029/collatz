c81038c40ecf36224d587756c28f1def46ed39d2
ssss1029 <sauravkadavath@gmail.com>
2017-05-29 20:08:55 -0700

    finished rudimentary display

--- collatz.py
+++ collatz.py
@@ -1,5 +1,29 @@
-import matplotlib as pyplot
+import matplotlib.pyplot as pyplot
 
-plt.plot([1,2,3,4])
-plt.ylabel('some numbers')
-plt.show()
+def generateCollatzPlot(n): 
+	x_value = list(range(1,n))
+	num_iterations = []
+
+	for val in x_value:
+		num_iterations.append(numToFinishCollatz(val))
+
+	pyplot.plot(num_iterations)	
+	pyplot.ylabel('some numbers')
+	pyplot.show()
+
+def numToFinishCollatz(n):
+	# Only print out every 1000 numbers
+	if (n % 1000 == 0):
+		print("Starting collatz for number: " + str(n) )
+	
+	count = 0
+	while n != 1:
+		if n % 2 == 0:
+			n = n / 2
+		else:
+			n = 3*n + 1
+		count = count + 1
+	return count
+
+
+generateCollatzPlot(1000000)
